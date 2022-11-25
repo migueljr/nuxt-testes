f21c5560c5854c796c12611366aa39b18a5dfa1d
migueljr <miguel_jr@mail.com>
2022-11-25 14:21:53 -0300

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>About</h3>
+    <nuxt-link :to="'/'">home</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>home</h3>
+    <nuxt-link :to="'/about'">about</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
